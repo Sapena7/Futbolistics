6db0cd6a6acf5b27c21dc6cde7a2dde4a0f8feff
Sapena7 <jaumesapena77@gmail.com>
2020-04-16 16:59:25 +0200

    twig

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -8,11 +8,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class DefaultController extends AbstractController
 {
     /**
-     * @Route("/default", name="default")
+     * @Route("/", name="homepage")
      */
     public function index()
     {
-        return $this->render('default/index.html.twig', [
+        return $this->render('pagina/amateurs.html.twig', [
             'controller_name' => 'DefaultController',
         ]);
     }
