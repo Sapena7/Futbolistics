bdd33c830d42dfc853944c68cd4afd2e5a951491
Sapena7 <jaumesapena77@gmail.com>
2020-05-22 18:12:32 +0200

    Retoques

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -26,11 +26,11 @@ class RegistrationFormType extends AbstractType
                 'mapped' => false,
                 'constraints' => [
                     new NotBlank([
-                        'message' => 'Please enter a password',
+                        'message' => 'Por favor, introduce una contraseña',
                     ]),
                     new Length([
                         'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        'minMessage' => 'Tu contraseña debe tener mínimo {{ limit }} caracteres',
                         // max length allowed by Symfony for security reasons
                         'max' => 4096,
                     ]),
